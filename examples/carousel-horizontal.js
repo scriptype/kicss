58adefe2324d37167ee68a1e5cbd682b913c27a0
scriptype <scriptyper@gmail.com>
2020-12-16 02:09:17 +0200

    Improve carousel demo using interpolations
    - Now it doesn't need an apology
    - Number of items can be dynamic, without needing to change anything in css
    - Tab buttons can have different widths

--- examples/carousel-horizontal.js
+++ examples/carousel-horizontal.js
@@ -1,6 +1,7 @@
 ;(() => {
-  const tabs = document.querySelectorAll('.tab')
-  const panels = document.querySelectorAll('.carousel-item')
+  const tabsContainer = document.querySelector('.tabs')
+  const tabs = Array.from(document.querySelectorAll('.tab'))
+  const panels = Array.from(document.querySelectorAll('.carousel-item'))
   tabs.forEach((tab, index) => {
     tab.addEventListener('click', () => {
       panels[index].scrollIntoView()
@@ -8,11 +9,20 @@
   })
 
   const { reportVariable, reportScroll } = window.InteractionReporter
+  reportVariable('--items', panels.length)
+  reportVariable('--tabline-position', tabs[0].offsetLeft + tabs[0].offsetWidth / 2)
+  reportVariable('--active-tab-width', tabs[0].offsetWidth)
+
   const carousel = document.querySelector('.carousel')
   carousel.addEventListener('scroll', reportScroll({
     direction: 'horizontal',
-    name: '--carousel-scroll'
+    name: '--carousel-scroll',
+    interpolations: [
+      {
+        name: '--tabline-position',
+        inputRange: panels.map((panel, i) => panel.offsetWidth * i),
+        outputRange: tabs.map((tab) => tab.offsetLeft + tab.offsetWidth / 2)
+      }
+    ]
   }))
-
-  reportVariable('--items', panels.length)
 })()
