8c9f350cc52126a7a0359266b801dccc8b24fd84
scriptype <scriptyper@gmail.com>
2020-12-21 22:07:49 +0200

    Add tabs to scroll-content

--- examples/scroll-content.js
+++ examples/scroll-content.js
@@ -1,8 +1,30 @@
 ;(() => {
-  const { reportScroll } = window.InteractionReporter
+  const tabs = Array.from(document.querySelectorAll('.tab'))
+  const panels = Array.from(document.querySelectorAll('.panel'))
+
+  tabs.forEach((tab, index) => {
+    tab.addEventListener('click', () => {
+      panels[index].scrollIntoView()
+    })
+  })
+
+  const { reportVariable, reportScroll } = window.InteractionReporter
+  reportVariable('--tabline-position', tabs[0].offsetTop - tabs[0].offsetHeight / 2)
+
   const slider = document.querySelector('.content')
   slider.addEventListener('scroll', reportScroll({
     direction: 'vertical',
-    name: '--slider-scroll'
+    name: '--slider-scroll',
+    interpolations: [
+      {
+        name: '--tabline-position',
+        inputRange() {
+          return panels.map((panel, i) => panel.offsetTop)
+        },
+        outputRange() {
+          return tabs.map((tab) => tab.offsetTop - tab.offsetHeight / 2)
+        }
+      }
+    ]
   }))
 })()
